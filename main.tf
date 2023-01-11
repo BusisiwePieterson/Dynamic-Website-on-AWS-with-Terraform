3cf61bdc7d67bad49854fa41b9ad48317f02846f
Busi <pietersonbusi@gmail.com>
2023-01-11 14:10:34 +0200

    changes made

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "aws" {
   profile = "terraform-user"
 }
 
-# Store terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket = "busi-terraform-state-bucket"
-    key    = "fleetcart/terraform.tfstate"
-    region = "us-east-1"
-    profile = "terraform-user"
-  }
-}
+# # Storing terraform state file in s3
+# terraform {
+#   backend "s3" {
+#     bucket = "busi-terraform-state-bucket"
+#     key    = "fleetcart/terraform.tfstate.dev"
+#     region = "us-east-1"
+#     profile = "terraform-user"
+#   }
+# }
 
 
